c37eb08ec782ce838f85594b2833e1e2c4cf2f3f
famirah meidita <meiditafamirah@yahoo.com>
2016-03-09 22:41:16 +0700

    Data 1

---
+++ src/Mainkaryawan.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mainkaryawan;
+
+/**
+ *
+ * @author Meidita
+ */
+
+public class Mainkarwayan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    
+    System.out.println("DATA KARYAWAN");
+student paijo = new karyawan();
+paijo.setName("paijo");
+paijo.setNIK ("101");
+paijo.setgaji(900);
+paijo.setTunjangan(200);
+paijo.setBonus(150);
+paijo.setDenda(75);
+
+student lulu = new karyawan();
+lulu.setName("lulu");
+lulu.setNIK("102");
+lulu.setgaji(875);
+lulu.setTunjangan(200);
+lulu.setBonus(100);
+lulu.setDenda(100);
+
+student lala= new karyawan();
+lala.setName("lala");
+lala.setNIK("103");
+lala.setgaji(1000);
+lala.setTunjangan(350);
+lala.setBonus(200);
+lala.setDenda(125);
+
+
+    }
+    }
+
+    
